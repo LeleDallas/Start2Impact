8af8e3610a4bf907af4bf4825b4120f9915cfd8f
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-11-02 16:31:58 +0100

    refactor: update variable names, constant in function

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-function getCookie(cName) {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(document.cookie); //to be careful
-    const cArr = cDecoded.split('; ');
+
+function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const cookieDecoded = decodeURIComponent(document.cookie);
+    const cookieArray = cookieDecoded.split('; ');
     let res;
-    cArr.forEach(val => {
-        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    cookieArray.forEach(cookie => {
+        if (cookie.indexOf(name) === 0)
+            res = cookie.substring(name.length);
     })
     return res;
 }
 
-// Set a Cookie
-function setCookie(cName, cValue, expDays) {
+const milliSecondsInDay = 24 * 60 * 60 * 1000
+
+const calculateTokenExpireTime = (expireDays, date) => expireDays * milliSecondsInDay + date.getTime()
+
+function setCookie(cookieName, cookieValue, expireDays) {
     let date = new Date();
-    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    date.setTime(calculateTokenExpireTime(expireDays, date));
     const expires = "Expires=" + date.toUTCString();
-    document.cookie = cName + "=" + JSON.stringify(cValue) + "; " + expires + "; Path=/; Domain=.start2impact.it; ";
+    document.cookie = `${cookieName}=${JSON.stringify(cookieValue)}; ${expires}; Path=/; Domain=.start2impact.it`;
 }
 
 function setUtmCookie() {
-    let cName = 'utm_parameters';
-    if (getCookie(cName) != undefined) {
-        return;
-    }
-    let url = new URL(window.location);
-    let search_params = url.searchParams;
-    let utm_campaign = search_params.get('utm_campaign');
-    let utm_medium = search_params.get('utm_medium');
-    let utm_source = search_params.get('utm_source');
-    let expDays = 30;
-    let landed_at = new Date().toLocaleString(); //.toString();
-    let cValue = { utm_campaign, utm_medium, utm_source, landed_at };
+    const cookieName = 'utm_parameters';
+    if (getCookie(cookieName) != undefined) return;
+    const url = new URL(window.location);
+    const search_params = url.searchParams;
+    const utm_campaign = search_params.get('utm_campaign');
+    const utm_medium = search_params.get('utm_medium');
+    const utm_source = search_params.get('utm_source');
+    const expireDays = 30;
+    const landed_at = new Date().toLocaleString(); //.toString();
+    const cValue = { utm_campaign, utm_medium, utm_source, landed_at };
     if (utm_campaign || utm_medium || utm_source) {
         if (!(utm_campaign && utm_medium && utm_source)) {
             console.warn('UTM parameters missing:', { utm_campaign, utm_medium, utm_source });
         }
-        setCookie(cName, cValue, expDays);
+        setCookie(cookieName, cValue, expireDays);
     }
 }
+
 setUtmCookie();
